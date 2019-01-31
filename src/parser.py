7a6fff439fea275061120b68491600f358b56d43
Josh Morrow <josh@jcmorrow.com>
2019-01-31 17:15:10 -0500

    Separate tokenizer out into a separate file

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,4 @@
-import re
+from src.tokenizer import Tokenizer
 
 
 class Parser:
@@ -38,40 +38,6 @@ class Parser:
                     return (Parser(inner_list_contents).run(), tokens)
             inner_list_contents.append(next_token)
         raise Exception("Unterminated List")
-
-
-class Tokenizer:
-    TOKENS = [
-        ("\\(", "list_start"),
-        ("\\)", "list_end"),
-        ("\\b[a-zA-z]+\\b", "atom"),
-        ("\\b[0-9]+\\b", "integer"),
-        ("\\B[/+*-]\\B", "operator")
-    ]
-
-    def __init__(self, code):
-        self.code = code.strip()
-
-    def run(self):
-        tmp_code = self.code
-        tokens = []
-
-        while len(tmp_code):
-            (token, match) = self.next_token_from_code(tmp_code)
-            if match:
-                tokens.append((token[1], tmp_code[:match.end()]))
-                tmp_code = tmp_code[match.end():].strip()
-            else:
-                raise Exception("Unexpected Token: {}".format(tmp_code))
-
-        return tokens
-
-    def next_token_from_code(self, code):
-        for token in self.TOKENS:
-            match = re.match(re.compile(token[0]), code)
-            if match:
-                return (token, match)
-        return (None, None)
 
 
 def parse(code):
---
+++ src/tokenizer.py
@@ -0,0 +1,35 @@
+import re
+
+
+class Tokenizer:
+    TOKENS = [
+        ("\\(", "list_start"),
+        ("\\)", "list_end"),
+        ("\\b[a-zA-z]+\\b", "atom"),
+        ("\\b[0-9]+\\b", "integer"),
+        ("\\B[/+*-]\\B", "operator")
+    ]
+
+    def __init__(self, code):
+        self.code = code.strip()
+
+    def run(self):
+        tmp_code = self.code
+        tokens = []
+
+        while len(tmp_code):
+            (token, match) = self.next_token_from_code(tmp_code)
+            if match:
+                tokens.append((token[1], tmp_code[:match.end()]))
+                tmp_code = tmp_code[match.end():].strip()
+            else:
+                raise Exception("Unexpected Token: {}".format(tmp_code))
+
+        return tokens
+
+    def next_token_from_code(self, code):
+        for token in self.TOKENS:
+            match = re.match(re.compile(token[0]), code)
+            if match:
+                return (token, match)
+        return (None, None)
